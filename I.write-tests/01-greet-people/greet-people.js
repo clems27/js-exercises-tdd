1b4aed9e46302a31851de555678d68c32e7409ea
clement <clemohiomoje@gmail.com>
2019-02-24 00:04:52 +0000

    greet people comment line code update

--- I.write-tests/01-greet-people/greet-people.js
+++ I.write-tests/01-greet-people/greet-people.js
@@ -1,3 +1,11 @@
+/*
+  Let's trace this piece of code - what is the value of result with this input
+
+  var mentors = ['Irina', 'Ashleigh', 'Etza'];
+  var result = greetPeople(mentors)
+*/
+
+
 function greetPeople(people) {
     var greeting = 'Hello ';
 
@@ -9,24 +17,3 @@ function greetPeople(people) {
 }
 
 module.exports = greetPeople;
-
-/*
-  Let's trace this piece of code - what is the value of result with this input
-
-  var mentors = ['Irina', 'Ashleigh', 'Etza'];
-  var result = greetPeople(mentors)
-*/
-
-
-// function greetPeople(people) {
-//     var greeting = "Hello ";
-//     people.forEach(function(person,index){
-//         if(index !== people.length -1){
-// greeting =greeting + person + ", "
-//         }else{
-//             greeting = greeting + person;
-//         }
-//     })
-//     return greeting;
-// }
-// module.exports = greetPeople;
