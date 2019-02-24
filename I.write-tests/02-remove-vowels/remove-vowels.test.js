ad21f75d1b6ece3365f09102cdb930a5367d23a8
clement <clemohiomoje@gmail.com>
2019-02-24 00:15:23 +0000

    remove vowel test update

--- I.write-tests/02-remove-vowels/remove-vowels.test.js
+++ I.write-tests/02-remove-vowels/remove-vowels.test.js
@@ -10,9 +10,3 @@ test('remove vowels from word', function () {
     expect(result).toBe("sml");
    
 })
-
-// removeVowels = ["bongo drums", "guitar",
-//     "flute", "double bass", "xylophone", "piano"];
-// var stringsWithOutVovels = removeVowels.map(function (item) {
-//     return item.replace(/[aeiou]/g, '');
-//     console.log(stringsWithOutVovels);
