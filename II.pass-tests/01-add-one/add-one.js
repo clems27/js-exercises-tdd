ee14913cc17bf711ecb580a73c683539bb37e0ca
clement <clemohiomoje@gmail.com>
2019-02-24 00:19:25 +0000

    add one code refactoring update

--- II.pass-tests/01-add-one/add-one.js
+++ II.pass-tests/01-add-one/add-one.js
@@ -1,9 +1,4 @@
 module.exports = function (numbers) {
-        var result = [];
-
-    for (var i = 0; i < numbers.length; i++) {
-        var value = numbers[i];
-            result.push(value + 1);
-    }
-    return result;
+    return numbers.map(number => number + 1);
+    
 };
